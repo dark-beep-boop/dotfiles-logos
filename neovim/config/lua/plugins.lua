fcf4d0cb586cba43ad01dad5cfacfc370a663663
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2022-11-20 21:17:51 +0100

    nvim Packer used instead of vim-plug

--- neovim/config/lua/plugins.lua
+++ neovim/config/lua/plugins.lua
@@ -1,12 +1,3 @@
-vim.cmd([[
-  call plug#begin("~/.vim/plugged")
-    Plug 'bradcush/base16-nvim'
-    Plug 'ryanoasis/vim-devicons'
-    Plug 'SirVer/ultisnips'
-    Plug 'honza/vim-snippets'
-    Plug 'scrooloose/nerdtree'
-    Plug 'preservim/nerdcommenter'
-    Plug 'mhinz/vim-startify'
-    Plug 'neoclide/coc.nvim', {'branch': 'release'}
-  call plug#end()
-]])
+return require('packer').startup(function(use)
+  use 'bradcush/base16-nvim'
+  end)
