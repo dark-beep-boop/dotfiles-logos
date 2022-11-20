5fc2a0a48363640be4f39322494db14c7c41feea
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2022-11-20 23:20:57 +0100

    aesthetic plugins added

--- neovim/config/lua/plugins.lua
+++ neovim/config/lua/plugins.lua
@@ -1,8 +1,17 @@
 -- Use Packer as package manager
 return require("packer").startup(function(use)
-    use "bradcush/base16-nvim"
+    use { "bradcush/base16-nvim" }
     use {
       "kyazdani42/nvim-tree.lua", 
       requires = "kyazdani42/nvim-web-devicons"
+    }
+    use { "mhinz/vim-startify" }
+    use { "DanilaMihailov/beacon.nvim" }
+    use {
+      "nvim-lualine/nvim-lualine.nvim", 
+      requires = {
+        "kyazdani42/nvim-web-devicons",
+        opt = true
+      }
     }
   end)
