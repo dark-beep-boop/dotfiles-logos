f2b25c514786bab9555034ab361626cfa36bf566
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2022-11-20 21:25:31 +0100

    nvim-tree plugin added to neovim

--- neovim/config/lua/plugins.lua
+++ neovim/config/lua/plugins.lua
@@ -1,3 +1,7 @@
 return require('packer').startup(function(use)
-  use 'bradcush/base16-nvim'
+    use 'bradcush/base16-nvim'
+    use {
+      'kyazdani42/nvim-tree.lua', 
+      requires = 'kyazdani42/nvim-web-devicons'
+    }
   end)
