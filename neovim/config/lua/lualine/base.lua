664a684d30e9e9a7020a9317af33fc18e295e072
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2022-11-27 00:17:10 +0100

    evilline set as style

--- neovim/config/lua/lualine/base.lua
+++ neovim/config/lua/lualine/base.lua
@@ -1,7 +1,10 @@
-local base16_theme = require('lualine.themes.base16')
+local current_style = require('lualine.styles.evilline')
+local current_theme = require('lualine.themes.base16')
 
+require('lualine').setup(config)
 require('lualine').setup {
   options = {
-    theme = base16_theme
+    theme = current_theme
   }
 }
+
