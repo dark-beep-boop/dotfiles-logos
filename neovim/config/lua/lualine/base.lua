925cb03cc2fb0dd9e8d92046a8c31e4e99b399ad
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2022-11-27 02:17:56 +0100

    lualine configured

--- neovim/config/lua/lualine/base.lua
+++ neovim/config/lua/lualine/base.lua
@@ -1,10 +1,2 @@
-local current_style = require('lualine.styles.evilline')
 local current_theme = require('lualine.themes.base16')
-
-require('lualine').setup(config)
-require('lualine').setup {
-  options = {
-    theme = current_theme
-  }
-}
-
+require('lualine.styles.simple').setup_style(current_theme)
