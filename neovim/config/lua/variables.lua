caa1826ac490fe24b67359cb51b54782ca707668
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2022-11-30 00:37:48 +0100

    scratch window size fixed

--- neovim/config/lua/variables.lua
+++ neovim/config/lua/variables.lua
@@ -11,3 +11,12 @@ vim.o.packpath = vim.o.packpath .. "," .. packer_path
 
 -- [[ Deoplete ]]
 vim.api.nvim_command('let g:deoplete#enable_at_startup = 1')
+
+-- [[ Polyglot ]]
+vim.g.vim_markdown_folding_disabled = 1
+vim.g.vim_markdown_conceal = 0
+vim.g.vim_markdown_math = 1
+vim.g.vim_markdown_frontmatter = 1
+vim.g.vim_markdown_toml_frontmatter = 1
+vim.g.vim_markdown_json_frontmatter = 1
+vim.g.tex_conceal = ""
