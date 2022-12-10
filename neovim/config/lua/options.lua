03eb29c360bb544d908d9ebcc13c9af1f3ef5acf
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2022-12-10 21:13:41 +0100

    textwidth

--- neovim/config/lua/options.lua
+++ neovim/config/lua/options.lua
@@ -31,6 +31,8 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
 vim.opt.autoindent = true
+vim.opt.textwidth = 80
+vim.opt.formatoptions:append('a')
 
 -- [[ Movement ]]
 vim.opt.mouse = 'a'
