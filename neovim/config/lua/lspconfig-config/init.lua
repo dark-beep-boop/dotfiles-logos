a9dba9a924895bc69fe2e95283c74e8463430f95
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2023-04-01 19:27:22 +0200

    lspconfig config cleaned

--- neovim/config/lua/lspconfig-config/init.lua
+++ neovim/config/lua/lspconfig-config/init.lua
@@ -75,18 +75,6 @@ local servers = {
     latexindent = {
       modifyLineBreaks = false
     }
-  },
-
-  sumneko_lua = {
-    Lua = {
-      runtime = { version = 'LuaJIT' },
-      diagnostics = { globals = { 'vim' } },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
-      },
-      telemetry = { enable = false }
-    }
   }
 }
 
