81acd2720f9966056df9335328801017f546bfad
Jesús María Gómez Moreno <zus@gomoreno.me>
2025-07-09 10:12:11 +0200

    nvim config updated

--- neovim/config/lua/plugins/dap/dap-adapters/debugpy.lua
+++ neovim/config/lua/plugins/dap/dap-adapters/debugpy.lua
@@ -3,7 +3,7 @@
 return {
 	dependencies = { 'mfussenegger/nvim-dap-python' },
 	config = function()
-		require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+		require('dap-python').setup('/home/zus/.virtualenvs/debugpy/bin/python')
 		require('dap-python').test_runner = 'pytest'
 
 		vim.keymap.set('n', '<leader>dn', require('dap-python').test_method)
